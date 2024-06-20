682e8b4f6e41e3aec8397c584390da2a806cc7d6
colesa13 <sweetcake61091@yahoo.com>
2024-06-20 15:41:36 -0400

    updated styling and text files

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+//Import 
+import { htmlFrontPage, cssFrontPage, jsFrontPage } from "./modules/mainPageModes";
+
 const elements = document.getElementsByClassName('element');
+const mainDiv = document.getElementById('homeMainContent');
+
+
+
+const loadFrontPageText = function(obj) {
+    let subMainDiv = document.createElement('div');
+    subMainDiv.classList.add('homeMainText');
+    subMainDiv.innerHTML = `<h2>${obj.title}</h2> <p>${obj.description}</p> <img src='${obj.image}' alt='icon'>`;
+    mainDiv.appendChild(subMainDiv);
+}
 
 
 for(const element of elements) {
@@ -8,3 +21,7 @@ for(const element of elements) {
         element.children[0].children[2].classList.toggle('invisible');
     })
 }
+
+loadFrontPageText(htmlFrontPage);
+loadFrontPageText(cssFrontPage);
+loadFrontPageText(jsFrontPage);
